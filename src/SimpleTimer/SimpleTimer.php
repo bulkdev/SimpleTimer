7656d7f7101d4849dbe39daaa4b84e6dd6a0b262
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-10-29 21:50:04 -0400

    Create SimpleTimer.php

---
+++ src/SimpleTimer/SimpleTimer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SimpleTimer;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class SimpleTimer extends PluginBase implements Listener{
+
+public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function onJoin(PlayerJoinEvent $event){
+$player = $event->getPlayer();
+$this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this), 1 * 20);
+}
+
+}
